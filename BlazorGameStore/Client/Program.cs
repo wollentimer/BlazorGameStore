200968f5279f0ecd5ab432ff8fe268877de0b042
Николай Рузвельт <NikolayKusakin@yandex.by>
2022-10-12 20:43:34 +0300

    Add CategoryService with implementation

--- BlazorGameStore/Client/Program.cs
+++ BlazorGameStore/Client/Program.cs
@@ -1,4 +1,4 @@
-using BlazorGameStore.Client;
+using BlazorGameStore.Client.Services.CategoryService;
 using BlazorGameStore.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,9 +12,11 @@ namespace BlazorGameStore.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
             builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+
             await builder.Build().RunAsync();
         }
     }
