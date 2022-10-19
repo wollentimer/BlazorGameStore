974db80ac68d3dcf7972fbadbf63a4e452c7ff41
wollentimer <NikolayKusakin@yandex.by>
2022-10-19 18:25:52 +0300

    Add searchtext method

--- BlazorGameStore/Server/Services/ProductService/IProductService.cs
+++ BlazorGameStore/Server/Services/ProductService/IProductService.cs
@@ -5,7 +5,12 @@ namespace BlazorGameStore.Server.Services.ProductService
     public interface IProductService
     {
         Task<List<Product>> GetAllProductsAsync();
+        
         Task<List<Product>> GetProductsByCategoryAsync(string categoryUrl);
+
         Task<Product> GetProductAsync(int id);
+
+        Task<List<Product>> SearchProducts(string searchText);
+
     }
 }
